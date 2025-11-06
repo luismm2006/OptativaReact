020825a946f3d931b68d2aa3aafe42fa2f7f4193
luismm2006 <luismulamarquez@gmail.com>
2025-11-06 19:07:23 +0100

    Subida

--- RetoCronometro/vite-project/src/App.jsx
+++ RetoCronometro/vite-project/src/App.jsx
@@ -1,20 +1,44 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const timeoutRef = useRef(null);
+
+  timeoutRef.current = setTimeout(() => {
+      if (isRunning) {
+        setTime(time + 1);
+      }
+    }, 1000);
+
+  const startTimer = () => {
+    if (!isRunning) {
+      setIsRunning(true);
+    }
+  };
+
+  const stopTimer = () => {
+    setIsRunning(false);
+    clearTimeout(timeoutRef.current);
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    setTime(0);
+  };
 
   return (
     <>
-      <h1>Timer</h1>
-      {setTimeout(() => {
-        
-      }, timeout)}
-      <button>Start</button>
-      <button>Stop</button>
-      <button>Reset</button>
+      <h1>Timer (con setTimeout)</h1>
+      <h2>{time} s</h2>
+      <div className="buttons">
+        <button onClick={startTimer} disabled={isRunning}>Start</button>
+        <button onClick={stopTimer}>Stop</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
